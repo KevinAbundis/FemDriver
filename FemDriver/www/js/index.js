44e0ede2a6c332cd5fec31be6d63d6793594c51c
KeviinAbundiiz <44123659+KeviinAbundiiz@users.noreply.github.com>
2019-04-03 03:15:08 -0600

    Aplicación celular y página web

---
+++ FemDriver/www/js/index.js
@@ -0,0 +1,16 @@
+var btn = document.getElementById("confirmar");
+btn.addEventListener("click",confirmacion,false);
+
+
+
+function saludo() {
+	alert("GRACIAS POR ELEGIRNOS.");
+}
+
+
+function confirmacion() {
+	alert("Usuaria registrada exitosamente");
+}
+
+
+
