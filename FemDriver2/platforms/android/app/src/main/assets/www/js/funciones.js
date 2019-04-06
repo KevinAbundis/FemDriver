c01592668d96b6b10861b6e428b5d16b1a3e4218
KeviinAbundiiz <44123659+KeviinAbundiiz@users.noreply.github.com>
2019-04-06 13:32:09 -0600

    Versión 2

---
+++ FemDriver2/platforms/android/app/src/main/assets/www/js/funciones.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("cancelar");
+btn.addEventListener("click",saludo,false);
+
+
+
+function saludo() {
+	alert("Cancelación con éxito");
+}
---
+++ FemDriver2/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,16 @@
+var btn = document.getElementById("confirmar");
+btn.addEventListener("click",confirmacion,false);
+
+
+
+function saludo() {
+	alert("GRACIAS POR ELEGIRNOS.");
+}
+
+
+function confirmacion() {
+	alert("Usuario registrado exitosamente");
+}
+
+
+
